fe27fb56026d36da4b178d29ec3900c6e4f77abe
putbullet <soulaimanettabaas@gmail.com>
2024-04-21 18:11:43 +0100

    comm

--- models/user.py
+++ models/user.py
@@ -1,26 +1,19 @@
-#!/usr/bin/python3
-""" holds class User"""
-import models
+#!/usr/bin/python
+""" holds class City"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+from os import environ
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if (storage_engine == 'db'):
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    if environ.get("HBNB_TYPE_STORAGE") == 'db':
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
