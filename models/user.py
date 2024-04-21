c463871fa7408c117bdb3b56bfc201f028e31d1d
putbullet <soulaimanettabaas@gmail.com>
2024-04-21 18:08:00 +0100

    COM

--- models/user.py
+++ models/user.py
@@ -24,7 +24,3 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
