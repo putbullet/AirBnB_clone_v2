e313d1796cbc6dd81788c15aa117057f0328515e
putbullet <soulaimanettabaas@gmail.com>
2024-04-29 21:57:43 +0100

    Commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+#setup inital server conf
+if [ ! -x /usr/sbin/nginx ]; then
+        apt-get update
+        apt-get install nginx -y
+fi
+mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "fake content" | sudo tee /data/web_static/releases/test/index.html
+ln -nsf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu:ubuntu -R /data/
+sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+service nginx restart
